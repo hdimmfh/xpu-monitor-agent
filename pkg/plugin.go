6737853238ddfcf89840242a57051d6c1d7372c4
Myeongsu Koo <74033655+hdimmfh@users.noreply.github.com>
2026-07-10 10:37:01 +0900

    Rename  plugin.go

--- pkg/plugin.go
+++
@@ -1,5 +0,0 @@
-type Plugin interface {
-    Name() string
-    Discover(ctx context.Context) ([]Device, error)
-    Collect(ctx context.Context, deviceID string) ([]Metric, error)
-}
---
+++ pkg/plugin/plugin.go
@@ -0,0 +1,11 @@
+// pkg/plugin/plugin.go
+package plugin
+
+import "context"
+
+type Plugin interface {
+	Name() string
+	Discover(ctx context.Context) ([]Device, error)
+	Capabilities(ctx context.Context, deviceID string) ([]Capability, error)
+	Collect(ctx context.Context, deviceID string) ([]Metric, error)
+}
