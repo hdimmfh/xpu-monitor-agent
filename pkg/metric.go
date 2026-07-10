1c0e3e4b41c92a02528bb9082b7237db5f4271f0
Myeongsu Koo <74033655+hdimmfh@users.noreply.github.com>
2026-07-10 10:34:34 +0900

    Rename metric.go

--- pkg/metric.go
+++ pkg/metric.go
@@ -1,6 +1,11 @@
+// pkg/plugin/metric.go
+package plugin
+
+import "time"
+
 type Metric struct {
-    Name      string
-    Value     any
-    Unit      string
-    Timestamp time.Time
+	Name      string
+	Value     any
+	Unit      string
+	Timestamp time.Time
 }
